105d9b96dbf7599efa3de74046777d8580f627aa
NanopassDeveloper <hello@nanopass.io>
2021-12-26 20:33:40 +0000

    Update compare

--- scripts/compare.js
+++ scripts/compare.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const preReveal = require('../output/snapshot_prereveal.json')
 const postReveal = require('../output/snapshot.json')
 
@@ -25,14 +26,10 @@ for(const address of Object.keys(preRH)) {
 console.log(Object.keys(holders).length)
 console.log(Object.keys(holders).reduce((p,c) => p+holders[c], 0))
 
+
 let addresses = []
 for(const address of Object.keys(holders)) {
-  addresses.push({ address, count: holders[address] })
+  addresses.push({ address, balance: holders[address] })
 }
 
-addresses = addresses.sort((a,b) => b.count - a.count)
-addresses = addresses.slice(0, 10)
-
-console.log(JSON.stringify(addresses, null, 2))
-
-console.log(holders['0x2D7BFbA6e49c9cd451C44d27775725fc56F3B044'])
+fs.writeFileSync('../output/xmas_bb.json', JSON.stringify(addresses))
