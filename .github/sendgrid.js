0b200d114676b2245e52805187f1bb7289559006
NanopassDeveloper <hello@nanopass.io>
2022-01-25 03:00:44 +0000

    Send csv action

---
+++ .github/sendgrid.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { generateBatchCsv } = require("../public/js/functions")
+require("./functions/get-batch").handle().then(rawData => {
+  const filename = 'batch.csv',
+  fileType = 'text/csv',
+  data = generateBatchCsv(rawData);
+
+  const msg = {
+    to: process.env.EMAILS.split(','),
+    from: 'hello@nanopass.io',
+    subject: 'Black Box CSV',
+    text: 'Attached',
+    attachments: [
+        {
+            content: data.toString('base64'),
+            filename: filename,
+            type: fileType,
+            disposition: 'attachment',
+        },
+    ],
+  };
+
+  sgMail
+    .send(msg)
+    .then(() => console.log('Mail sent successfully'))
+    .catch(error => console.error(error.toString()));
+})
