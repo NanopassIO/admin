5a0a7ad7984e19e2d0ed0b60c7e5a7b66af18c80
NanopassDeveloper <hello@nanopass.io>
2022-02-06 13:10:06 +0000

    Add auto test

---
+++ e2e/activate-auto.test.js
@@ -0,0 +1,46 @@
+const preload = require('../functions/preload-batch-auto-background').handle
+const activate = require('../functions/activate-batch-auto-background').handle
+const addPrize = require('../functions/add-prize').handle
+const { MockDB, MockContractor } = require('./testing')
+
+var crypto = require('crypto')
+
+async function countBoxes(db, batch) {
+  const addresses = await db.query('batches', 'batch', batch)
+  let boxCount = 0
+  for(const address of addresses.Items) {
+    boxCount += address.balance
+  }
+  return boxCount
+}
+
+it("activate batch after preload", async () => {
+  const addresses = []
+  for(let i = 0;i < 5555;i++) {
+    var id = crypto.randomBytes(20).toString('hex');
+    var testAddress = "0x"+id
+    addresses.push(testAddress)
+  }
+  const db = new MockDB()
+  const contract = new MockContractor(addresses)
+
+  await db.put('settings', { active: 'active', batch: 'batch-0' })
+
+  await preload(undefined, db, contract)
+
+  expect(await countBoxes(db, 'test')).toStrictEqual(5555)
+
+  await addPrize({
+    batch: 'test',
+    name: 'Test',
+    description: 'Test',
+    image: 'test.png',
+    count: 100
+  }, db)
+
+  const addresses2 = addresses.slice(0, 4000)
+  const contract2 = new MockContractor(addresses2)
+  await activate(undefined, db, contract2)
+
+  expect(await countBoxes(db, 'test')).toStrictEqual(4000)
+})
