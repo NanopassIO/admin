36eea97307986348965e72b8fcf3d810bbe5b382
NanopassDeveloper <hello@nanopass.io>
2022-03-02 17:45:44 +0000

    Optimize snapshots

--- src/eth.js
+++ src/eth.js
@@ -1,17 +1,38 @@
 const ABI = require('./abi.json')
+const SNAPSHOT_ABI = require('./snapshotAbi.json')
 const { ethers } = require('ethers')
 const retry = require('async-retry')
 
 const MAX_CONCURRENCY = 200
 const CONTRACT_ADDRESS = '0xf54cc94f1f2f5de012b6aa51f1e7ebdc43ef5afc'
+const SNAPSHOT_ADDRESS = '0xf3fB3F2Dab388Dc0d868be4A349aa1e8939D315D'
 
 exports.createContract = function () {
   const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_API_KEY)
   return new ethers.Contract(CONTRACT_ADDRESS, ABI, provider)
 }
 
+const createSnapshotContract = function () {
+  const provider = new ethers.providers.InfuraProvider('mainnet', process.env.INFURA_API_KEY)
+  return new ethers.Contract(SNAPSHOT_ADDRESS, SNAPSHOT_ABI, provider)
+}
+
 exports.takeSnapshot = async function(contract) {
+  let snapshotContract = createSnapshotContract()
+
   const maxSupply = 5555
+  const portions = 200
+  
+  if(contract.address) {
+    const concurrency = Math.ceil(maxSupply / portions)
+    console.log(`Concurrency is ${concurrency}`)
+    const section = Array.from(Array(concurrency), (_,x)=> x)
+    return (await Promise.all(section.map(async x => {
+        return await snapshotContract.takeSnapshot(contract.address, x * portions, 
+          Math.min(x * portions + portions, maxSupply))
+      }))).flat()
+  }
+
   let snapshot = []
   for(let i = 0;i < maxSupply;i+=MAX_CONCURRENCY) {
     const section = Array.from(Array(MAX_CONCURRENCY), (_,x)=>i+x).filter(x => x < maxSupply)
