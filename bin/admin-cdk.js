fe3fc458db3cec2c0033e48a2b8ecc9a3ce8f7bd
bazooka9966 <bazooka9966@gmail.com>
2022-10-06 01:17:10 +0800

    Rename the cdk file

--- bin/admin-cdk.js
+++ bin/admin-cdk.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const cdk = require('aws-cdk-lib')
-const { HelloCdkStack } = require('../lib/hello-cdk-stack')
+const { AdminCdkStack } = require('../lib/admin-cdk-stack')
 
 const app = new cdk.App()
-new HelloCdkStack(app, 'HelloCdkStack', {
+new AdminCdkStack(app, 'AdminCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
