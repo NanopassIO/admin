4250dd739ec8af21ccbbbdae78e70c343b6972d1
bazooka9966 <bazooka9966@gmail.com>
2022-10-27 09:21:57 +0800

    Add the deployment instruction and script

--- bin/admin-cdk.js
+++ bin/admin-cdk.js
@@ -4,6 +4,7 @@ const cdk = require('aws-cdk-lib')
 const { AdminCdkStack } = require('../lib/admin-cdk-stack')
 
 const app = new cdk.App()
+require('dotenv').config()
 new AdminCdkStack(app, 'AdminCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -13,6 +14,6 @@ new AdminCdkStack(app, 'AdminCdkStack', {
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: process.env.ACCOUNT_NO, region: process.env.REGION }
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 })
